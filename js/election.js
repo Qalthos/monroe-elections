d61ce4b99f0bd8910ab044e3c033a87c9d03adaa
Nathaniel Case <Qalthos@gmail.com>
2012-06-14 14:26:46 -0400

    Pull interval code from where it was hiding on foss

--- js/election.js
+++ js/election.js
@@ -10,17 +10,28 @@ function contestLoad() {
         $(".IND").children('span').progressBar({ barImage: 'images/progressbg_Independence.png'} );
         $(".OTHER").children('span').progressBar({ barImage: 'images/progressbg_Other.png'} );
     });
+    var clicked = $(this)
+    if (interval != null) {
+            clearInterval(interval);
+    }
+    interval = setInterval(function() {
+            clicked.click()
+    }, 60000);
     return false;
 }
 
 function areaLoad() {
     var toLoad = 'area.html #a'+$(this).attr('id');
-    $('#area').load(toLoad, function(data, status, xhr) {
+    $('#area').load(toLoad, function(data) {
         $(".loadC").click(contestLoad);
     });
     return false;
 }
 
 $(document).ready(function() {
+    setInterval(function() {
+        $('progress').load('index.html #in_progress');
+    }, 60000);
+    interval = null;
     $(".loadA").click(areaLoad);
 });
