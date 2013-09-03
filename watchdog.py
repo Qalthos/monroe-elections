5eea6e6d87c1629a5294e61db9f1f0cd46d2eacb
Nathaniel Case <Qalthos@gmail.com>
2013-09-03 13:17:42 -0400

    Watchdog process to spin up elections.

---
+++ watchdog.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+#
+# This code is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This code is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this code.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author: Nathaniel Case
+
+""" Watchdog is a part of Monroe Election Dashboard."""
+from __future__ import print_function
+from datetime import datetime
+
+import bs4
+import requests
+import sh
+import yaml
+
+with open('config.yaml') as config_file:
+    conf = yaml.load(config_file)
+
+now = datetime.now()
+for county in conf:
+    if conf[county].get('url'):
+        url = conf[county]['url'] + 'ElectionEvent.xml'
+        try:
+            html = requests.get(url).content
+        except:
+            continue
+        soup = bs4.BeautifulSoup(html)
+        date = soup.find('election')['dt']
+        date_parsed = datetime.strptime(date, '%Y%m%d').replace(hour=12)
+        if date_parsed > now:
+            # Parse the output of `atq`:
+            # The output consists of `jobid date queue user`, but date has
+            # lots of whitespace, so we split, throw away the ends, and join
+            # it again.
+            jobs = [' '.join(bits.split()[1:-2]) for bits in sh.atq().split('\n') if bits]
+            for job in jobs:
+                if datetime.strptime(job, '%a %b %d %H:%M:%S %Y') == date_parsed:
+                    # Job already exists
+                    break
+            else:
+                print("There's a new election coming up in {} at {}!".format(county, date_parsed))
+                sh.at('-t', '{}1200'.format(date), _in="python election.py -l")
