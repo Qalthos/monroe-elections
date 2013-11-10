e5f05aa1270d33f19a2d5502993b121cde2315d0
Nathaniel Case <Qalthos@gmail.com>
2013-11-10 00:04:31 -0500

    Try not to write empty files.

--- election.py
+++ election.py
@@ -141,12 +141,10 @@ class Election(object):
 
         try:
             r = requests.get(url)
-            if not r.status_code == 200:
-                # Can't get it
-                return filepath
-            with open(filepath, 'w') as out_file:
-                out_file.write(r.content)
-            commitAll(self.filepath)
+            if r.status_code == 200 and r.content:
+                with open(filepath, 'w') as out_file:
+                    out_file.write(r.content)
+                commitAll(self.filepath)
         except requests.exceptions.ConnectionError:
             # Connection timed out, use the last one we have
             pass
