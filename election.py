a38e4c7f2bc985ea10884aac484b80e58e8eb8f8
Nathaniel Case <Qalthos@gmail.com>
2015-09-10 15:01:50 -0400

    2015 Primary location update.

--- election.py
+++ election.py
@@ -31,8 +31,8 @@ from view import write_html, write_json, tabs, clear_tabs
 
 BASE_URLS = {
     'monroe': 'http://enr.monroecounty.gov/',
-    'suffolk': 'http://apps.suffolkcountyny.gov/boe/eleres/14ge/',
-    'chautauqua': 'http://app.co.chautauqua.ny.us/BOE/Results/',
+    'suffolk': 'http://apps.suffolkcountyny.gov/boe/eleres/15pe/',
+    'chautauqua': 'http://72.45.245.14/BOE/Results/',
     'orange': 'http://boe.co.orange.ny.us/',
 }
 
